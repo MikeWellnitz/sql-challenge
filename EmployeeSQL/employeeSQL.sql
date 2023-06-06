67f10d24636a4eb34b91f524624d8f3a522137f8
MikeWellnitz <dognutz780@yahoo.com>
2023-06-06 17:12:15 -0500

    Adding completed Homework files

--- EmployeeSQL/employeeSQL.sql
+++ EmployeeSQL/employeeSQL.sql
@@ -52,20 +52,70 @@ create table dept_manager (
 	PRIMARY KEY (dept_no, emp_no)  --composite key
 );
 
+
+
+--List the employee number, last name, first name, sex, and salary of each employee.
 select employees.emp_no, last_name, first_name, sex, salary
 from employees 
 left join salaries on employees.emp_no = salaries.emp_no;
 
 
-
-
---select *
---from employees where extract (year from hire_date) = 1986 and sex = 'M' and first_name like 'A%';
-
+--List the first name, last name, and hire date for the employees who were hired in 1986.
 select first_name, last_name, hire_date 
 from employees where extract (year from hire_date) = 1986;
 
-select first_name, last_name, hire_date 
-from employees where extract (year from hire_date) = 1986;
+--List the manager of each department along with their department number, department name, employee number, last name, and first name.
+select titles.title, departments.dept_no, departments.dept_name, employees.emp_no, employees.last_name, employees.first_name
+from employees
+join titles on employees.emp_title_id = titles.title_id
+join dept_manager on employees.emp_no = dept_manager.emp_no
+join departments on dept_manager.dept_no = departments.dept_no;
+
+SELECT 
+	titles.title
+	, departments.dept_no
+	, departments.dept_name
+	, employees.emp_no
+	, employees.last_name
+	, employees.first_name
+FROM employees
+JOIN titles
+	ON employees.emp_title_id = titles.title_id
+JOIN dept_manager
+	ON employees.emp_no = dept_manager.emp_no
+JOIN departments 
+	ON dept_manager.dept_no = departments.dept_no;
 
 
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+select dept_emp.dept_no, employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+join dept_emp on employees.emp_no = dept_emp.emp_no
+join departments on dept_emp.dept_no = departments.dept_no;
+
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+select first_name, last_name, sex
+from employees where first_name = 'Hercules' and last_name like 'B%';
+
+
+--List each employee in the Sales department, including their employee number, last name, and first name.
+select departments.dept_name, employees.emp_no, employees.last_name, employees.first_name
+from employees 
+join dept_emp on employees.emp_no = dept_emp.emp_no
+join departments on dept_emp.dept_no = departments.dept_no
+where dept_name = 'Sales';
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select departments.dept_name, employees.emp_no, employees.last_name, employees.first_name
+from employees 
+join dept_emp on employees.emp_no = dept_emp.emp_no
+join departments on dept_emp.dept_no = departments.dept_no
+where dept_name = 'Sales' or dept_name = 'Development';
+
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+select last_name, count(last_name) as frequency
+from employees
+group by last_name
+order by frequency desc;
